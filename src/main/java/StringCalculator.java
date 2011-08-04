1c228dbbc8cef219fdbcfbc81475b967bdb1f78b
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-08-04 21:41:57 +0200

    Initilized project structure and added empty class "StringCalc"

---
+++ src/main/java/StringCalculator.java
@@ -0,0 +1,8 @@
+/**
+ * Calculate sums based on String input.
+ *
+ * A video of this kata can be found on {@link http://osherove.com/tdd-kata-1}, with an
+ * step-by-step How-To at the bottom.
+ */
+public class StringCalculator {
+}
