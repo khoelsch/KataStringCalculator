4d65fe74cae636d5d2667d753a76a09b8dfee82f
Kai Hoelscher <Kai.Hoelscher@coremedia.com>
2011-08-05 08:35:02 +0200

    Step 3

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,4 +1,5 @@
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Calculate sums based on String input.
@@ -7,24 +8,66 @@ import java.util.Set;
  */
 public class StringCalculator {
 
-  private final static String NUMBER_DELIMITER = ",";
+  private final static String[] DELIMITERS = {",", "\n"};
+
+  static private boolean containsDelimiter(final String number) {
+    int delimIndex = 0;
+    while (delimIndex < DELIMITERS.length) {
+      if (number.contains(DELIMITERS[delimIndex])) {
+        return true;
+      }
+      ++delimIndex;
+    }
+    return false;
+  }
+
+  static private int firstHit(final String number) {
+    for (int i = 0; i < DELIMITERS.length; ++i) {
+      if (number.contains(DELIMITERS[i])) {
+        return number.indexOf(DELIMITERS[i]);
+      }
+    }
+
+    return -1;
+  }
+
+  static private List<String> splitNumber(final String number) {
+    List<String> numbers = new ArrayList<String>();
+    String rest = number;
+    int hitPos = 0;
+
+    while (containsDelimiter(rest)) {
+      hitPos = firstHit(rest);
+      numbers.add(rest.substring(0, hitPos));
+      rest = rest.substring(hitPos+1);
+    }
+
+    //add last number, not delimiters left
+    numbers.add(rest);
+
+    return numbers;
+  }
 
   static int add(final String number) {
-    if ("".equals(number) || "0".equals(number))
+    if ("".equals(number) || "0".equals(number)) {
       return 0;
+    }
 
     try {
-      if (!number.contains(NUMBER_DELIMITER)) {
+      if (!containsDelimiter(number)) {
         return Integer.valueOf(number).intValue();
       }
 
-      final String[] numbers = number.split(NUMBER_DELIMITER);
+      final List<String> numbers = splitNumber(number);
       int sum = 0;
-      for (int i=0 ; i<numbers.length ; ++i)
-        sum += Integer.valueOf(numbers[i]).intValue();
+      for (String num : numbers) {
+        sum += Integer.valueOf(num).intValue();
+      }
       return sum;
 
     } catch (NumberFormatException ex) {
+      throw new IllegalArgumentException("This kata is only designed for valid inputs!");
+    } catch (Throwable ex) {
       throw new IllegalArgumentException("This kata is only designed for valid inputs!");
     }
   }
